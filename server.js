d5342def064b4fed180b27562efa9fe12bd29b20
Abstrack <Armon_ah05@outlook.com>
2022-05-24 18:41:14 -0400

    added connection to db

--- server.js
+++ server.js
@@ -1,11 +1,29 @@
 const express = require('express');
+const mysql = require('mysql2');
+const config = require('config');
+
+const user = config.get('server.user');
+const pw = config.get('server.password');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+// Express Middlewares
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// connecting to Database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: user,
+        password: pw,
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+// test server connection (client side)
 // app.get('/' , (req, res) => {
 //     res.json({message: 'Hello MOTO!'});
 // });
